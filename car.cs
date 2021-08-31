774cc10edd042789c887eba486871260a7e37380
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-08-31 14:48:06 +0530

    add carprogrm after ignore

---
+++ car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp058
+{
+    class Car
+    {
+        string color = "SKYBLUE";
+        int model = 40788;
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+
+        }
+        public static void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.model);
+            myObj.Drive();
+            Car.Brake();
+            Console.Read();
+        }
+    }
+}
